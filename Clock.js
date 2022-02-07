a14b753aa48c87f5b0e4ebce8d8ea110cfe12701
Pia Lopez Copetti <60473265+plopezcopetti@users.noreply.github.com>
2022-02-07 08:58:18 -0300

    updated clock

--- Clock.js
+++ Clock.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Clock extends React.Component {
-  // Add your methods in here.
-  render() {
-    return <div>{this.state.date.toLocaleTimeString()}</div>;
-  }
   constructor(props) {
     super(props);
     this.state = { date: new Date() };
+  }
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+  componentDidMount() {
+    const oneSecond = 1000;
+    setInterval(() => {
+      this.setState({ date: new Date() });
+    }, oneSecond);
   }
 }
 
